3f49ba80f60670972e8aeda79416030633f14162
quy10 <quy10@DESKTOP-BG6FA4L>
2024-02-24 08:39:20 +0700

    Add project files.

---
+++ BanDienThoai/Controllers/FileAndMailController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanDienThoai.Controllers
+{
+    public class FileAndMailController : Controller
+    {
+        // GET: FileAndMail
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
